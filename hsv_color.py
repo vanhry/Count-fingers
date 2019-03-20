ff508e6bece5570680863afdb8352a4d25f53f5a
vanhry <vanhry@gmail.com>
2019-03-20 19:06:30 +0200

    for adjust color ranges

--- hsv_color.py
+++ hsv_color.py
@@ -1,48 +1,57 @@
 import numpy as np
 import cv2
+from sklearn.cluster import DBSCAN
 
-
+# function for trackbar
 def nothing(x):
     pass
 
-cv2.namedWindow('image')
+# Create trackbars for adjust hsv ranges
+
+cv2.namedWindow('image',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('image', 600,600)
 cv2.createTrackbar('H_min','image', 0,255,nothing)
 cv2.createTrackbar('H_max','image', 0,255,nothing)
 cv2.createTrackbar('S_min','image', 0,255,nothing)
 cv2.createTrackbar('S_max','image',0,255,nothing)
 cv2.createTrackbar('V_min','image',0,255,nothing)
 cv2.createTrackbar('V_max','image',0,255,nothing)
-# create switch for ON/OFF functionality
-switch = '0 : OFF \n1 : ON'
-cv2.createTrackbar(switch, 'image',0,1,nothing)
 
-
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 
 while True:
-    _, frame = cap.read()
-    # frame = cv2.imread('./memage2.jpg')
+    #_, frame = cap.read()
+    frame = cv2.imread('./memage_3f.jpg')
+    
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    # get trackbars for color change
 
-    # create trackbars for color change
     H_min = cv2.getTrackbarPos('H_min', 'image')
     S_min = cv2.getTrackbarPos('S_min', 'image')
     V_min = cv2.getTrackbarPos('V_min', 'image')
     H_max = cv2.getTrackbarPos('H_max', 'image')
     S_max = cv2.getTrackbarPos('S_max', 'image')
     V_max = cv2.getTrackbarPos('V_max', 'image')
-    s = cv2.getTrackbarPos(switch,'image')
 
+    # Color ranges for human skin
+    # (very sensitive from lightness and often need to adjust)
     lower = np.array([H_min, S_min, V_min])
     upper = np.array([H_max, S_max, V_max])
-    mask = cv2.inRange(hsv, lower, upper)
+    mask = cv2.GaussianBlur(hsv, (5, 5), 100)
+    mask = cv2.inRange(mask, lower, upper)
 
+    
+
+    # cv2.putText(frame, str(len(np.unique(labels))),(0,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 3, cv2.LINE_AA)
+    cv2.resizeWindow('image', 400,400)
     cv2.imshow('image', mask)
     cv2.imshow('frame', frame)
 
+    # Esc for exit
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
 
 cv2.destroyAllWindows()
-cap.release()
+#cap.release()

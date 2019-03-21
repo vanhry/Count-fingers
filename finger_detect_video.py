536ddc363f7b9243170de0dd4f895163b1562c7d
vanhry <vanhry@gmail.com>
2019-03-21 13:06:56 +0200

    almost done

--- finger_detect_video.py
+++ finger_detect_video.py
@@ -2,13 +2,20 @@ import numpy as np
 import cv2
 from sklearn.cluster import DBSCAN
 
-# function for trackbar
-def nothing(x):
-    pass
 
-# distance between two points and vectorize function for it
-distance = lambda i, j: np.linalg.norm(i-j)
-vect_dist = np.vectorize(distance)
+def distance(p1, p2):
+    """
+    distance between two points and vectorize function for it
+
+    Arguments:
+    p1 - first point, numpy array of int with shape (2,)
+    p2 - second point, numpy array of int with shape (2,)
+
+    Return:
+    distance
+    """
+    return np.linalg.norm(p1-p2)
+
 
 def check_for_maxhull(point, max_hull, measure):
     """
@@ -27,6 +34,7 @@ def check_for_maxhull(point, max_hull, measure):
             return False
     return True
 
+
 def per_distance(lp1, lp2, p):
     """
     Compute distance from two point line to one point
@@ -41,7 +49,7 @@ def per_distance(lp1, lp2, p):
     """
     return np.linalg.norm(np.cross(lp2-lp1, lp1-p))/np.linalg.norm(lp2-lp1)
 
-# find max and min in the sequence of point
+
 def peakdet(v, delta, x=None):
     """
     Finds the local maxima and minima ("peaks") in the vector V
@@ -105,58 +113,60 @@ def get_angle(p1, p2, p3):
     return angle * 180 / np.pi
 
 
-# built angles which describes count of fingers
 def find_nearest(means, roots, image):
-    means = sorted(means,key=lambda x:x[0])
-    roots = sorted(roots,key=lambda x:x[0])
+    """
+    Built angles between fingers and count fingers
+
+    Arguments:
+    means -- array of tuples (point on the finger's edge)
+    roots -- array of tuples (point between fingers)
+    image -- numpy array of shape (n, m, 3)
+
+    Return:
+    None
+    """
+    means = sorted(means, key=lambda x: x[0])
+    roots = sorted(roots, key=lambda x: x[0])
 
     means = np.array(means)
     roots = np.array(roots)
     n = len(means)-len(roots)
-    if n > 1:
-        for _ in range(n-1):
-	        roots = np.vstack([roots,roots[-1]])
+    #if n > 1:
+    #    for _ in range(n-1):
+    #        roots = np.vstack([roots, roots[-1]])
 
     count_finger = 0
     for i in range(len(means)-1):
-        angle = get_angle(roots[count_finger],means[i],means[i+1])
-        if (angle > 10 and angle <  80) and distance(roots[count_finger], means[i+1]) > 70 and distance(roots[count_finger], means[i])>70 and \
-            (means[i+1][1] < roots[count_finger][1] and means[i][1] < roots[count_finger][1]):
+        if i == len(roots):
+            break
+        angle = get_angle(roots[count_finger], means[i], means[i+1])
+
+        if angle > 10 and angle < 80 and distance(roots[count_finger], means[i+1]) > 70 \
+            and distance(roots[count_finger], means[i]) > 70 \
+            and means[i+1][1] < roots[count_finger][1] \
+            and means[i][1] < roots[count_finger][1]:
+
             cv2.line(image, tuple(roots[count_finger]), tuple(means[i+1]), (0,255,0),4)
             cv2.line(image, tuple(roots[count_finger]), tuple(means[i]), (0,255,0),4)
             count_finger += 1
 
     if count_finger >= 1 and count_finger <= 5:
         cv2.putText(frame, str(count_finger+1),(0,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 3, cv2.LINE_AA)
+    if count_finger == 0:
+        cv2.putText(frame, str(1),(0,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 3, cv2.LINE_AA)
 
-# Create trackbars for adjust hsv ranges
-"""
-cv2.namedWindow('image')
-cv2.createTrackbar('H_min','image', 0,255,nothing)
-cv2.createTrackbar('H_max','image', 0,255,nothing)
-cv2.createTrackbar('S_min','image', 0,255,nothing)
-cv2.createTrackbar('S_max','image',0,255,nothing)
-cv2.createTrackbar('V_min','image',0,255,nothing)
-cv2.createTrackbar('V_max','image',0,255,nothing)
-"""
+
+
 cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
-    # frame = cv2.imread('./memage2.jpg')
+
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # get trackbars for color change
-    """
-    H_min = cv2.getTrackbarPos('H_min', 'image')
-    S_min = cv2.getTrackbarPos('S_min', 'image')
-    V_min = cv2.getTrackbarPos('V_min', 'image')
-    H_max = cv2.getTrackbarPos('H_max', 'image')
-    S_max = cv2.getTrackbarPos('S_max', 'image')
-    V_max = cv2.getTrackbarPos('V_max', 'image')"""
 
     # Color ranges for human skin
-    # (very sensitive from brightness  and often need to adjust)
+    # (very sensitive from brightness  and often need to adjust in file hsv_color.py)
     lower = np.array([0, 0, 140])
     upper = np.array([255, 90, 255])
     mask = cv2.GaussianBlur(hsv, (5, 5), 100)
@@ -168,17 +178,12 @@ while True:
     if contours == []:
         continue
     max_contour = max(contours, key=lambda x: cv2.contourArea(x))
-    # try:
-    #     max_contour = max(contours, key=lambda x: cv2.contourArea(x))
-    # except ValueError:
-    #     print("Don't find max contour yet, maybe list of countour are empty")
-    #     continue
 
     cnt = contours[0]  # delete excess dim for list
     max_hull = cv2.convexHull(max_contour)
 
     # Approximate area of hand
-    epsilon = 0.005*cv2.arcLength(max_contour, True)
+    epsilon = 0.003*cv2.arcLength(max_contour, True)
     approx = cv2.approxPolyDP(max_contour, epsilon, True)
 
 
@@ -194,39 +199,34 @@ while True:
     # for throw out max hull points from bottom (not from fingers)
     center = np.mean(max_hull, axis=0, dtype=np.uint32)
     cx, cy = tuple(center[0])
-    cy += (delta * 2)  # little shift center to downside
-    cv2.circle(frame, (int(cx), int(cy)), 10, (255, 0, 255), 1)
+    cy += (delta*2.5 )  # little shift center to downside
+    # final = cv2.drawContours(frame, max_hull, -1, (255, 0, 0), 3)
+    # final = cv2.drawContours(frame, approx, -1, (0,0,0),5)
 
-    #final = cv2.drawContours(frame, max_hull, -1, (255, 0, 0), 3)
-    #final = cv2.drawContours(frame, approx, -1, (0,0,0),5)
 
     # find local min (points between fingers)
     # also remove points which close to max_hull
-    data = np.array(np.squeeze(approx,axis=1))[:,1]
+    data = np.array(np.squeeze(approx, axis=1))[:,1]
     indexes, _ = peakdet(data,.2)
     if indexes.size > 0:
         real_points = []
-        corner_points = np.take(np.array(np.squeeze(approx,axis=1)), indexes[:,0],axis=0)
+        corner_points = np.take(np.array(np.squeeze(approx, axis=1)), indexes[:,0], axis=0)
         for point in corner_points:
             if check_for_maxhull(point,max_hull,20) is True and point[1]<cy:
                 cv2.circle(frame, tuple(point), 5, (0, 255, 255),-1)
                 real_points.append(tuple(point))
     else:
-        cv2.putText(frame, 'None',(0,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 3, cv2.LINE_AA)
+        cv2.putText(frame, 'None', (0,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 3, cv2.LINE_AA)
         continue
-
 
     # For points on the top of fingers find nearest neighborhouds
     # join them into cluster and find mean of each
-
-
-    if real_points==[]:
+    if real_points == []:
         continue
-    min_root = min(real_points,key=lambda x:x[1])[1]
-
+    min_root = min(real_points, key=lambda x: x[1])[1]
     means = []
     # points on the edge of point never be smallest than root_points
-    max_hull_top = np.array([i[0] for i in max_hull if i[0][1] < min_root])
+    max_hull_top = np.array([i[0] for i in max_hull if i[0][1] < cy])
     if max_hull.shape[2] == 2:
         clustering = DBSCAN(eps=16, min_samples=1).fit(max_hull_top)
         labels = clustering.labels_
@@ -247,7 +247,6 @@ while True:
     if list(real_points) and means and len(means) >= len(real_points):
             find_nearest(means, real_points, frame)
 
-    # cv2.putText(frame, str(len(np.unique(labels))),(0,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 3, cv2.LINE_AA)
 
     cv2.imshow('image', mask)
     cv2.imshow('frame', frame)
@@ -256,6 +255,7 @@ while True:
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
+
 
 cv2.destroyAllWindows()
 cap.release()
